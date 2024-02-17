563f13d50ea1e8b7cb870abd5960df7b635ceefa
Khushishah2424 <khushiashah2004@gmail.com>
2024-02-17 16:22:32 +0530

    ...

--- c/Practice/reverse.c
+++ c/Practice/reverse.c
@@ -9,11 +9,22 @@
 
 
 #include<stdio.h>
+#define n  5
 int main(){
-    int a[5];
+    int a[n],temp;
     printf("Enter 5 integers :\n");
-    for(int i = 0 ; i < 5 ; i++){
+    for(int i = 0 ; i < n ; i++){
         scanf("%d",&a[i]);
     }
-
+    // 1 2 3 4 5 
+    for(int i = 0 ; i < n/2; i++){
+        temp = a[i]; 
+        a[i]= a[n - i - 1]; 
+        a[n - i - 1] = temp;
+    } printf("Reversed array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 }
+
---
+++ c/Practice/secondlargest.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#define n 5
+int main(){
+    int i,a[n],max,secondmax;
+    printf("Enter integers : \n");
+    for(i = 0 ; i < n ; i++){
+        scanf("%d",&a[i]);
+    }
+     
+    if(a[0] > a[1]){
+        max = a[0];
+        secondmax = a[1];
+    }
+    else{
+        max = a[1];
+        secondmax = a[0];
+    }
+
+    for(i = 2 ; i < n ; i++){
+        if(a[i] > max){
+            secondmax = max;
+            max = a[i];
+        }
+        else if(a[i] > secondmax && a[i]!= max){
+            secondmax = a[i];\
+        }
+    }
+    printf("%d",secondmax);
+    
+
+
+}
+
+
