cef264a30e466a287a2ddf7e1287164765268e97
Khushishah2424 <khushiashah2004@gmail.com>
2024-03-10 16:46:24 +0530

    ...

--- js/timeput/zomato.js
+++ js/timeput/zomato.js
@@ -18,10 +18,58 @@
 //         console.log("Delivered");
 //     },time);
 // }
+let orderedCancle = false;
+ function placeOrder(){
+    const status =  document.getElementById("status");
+    const orderPrep =  document.getElementById("orderPrep");
+    const delivery =     document.getElementById("delivery");
+    const limit = document.getElementById("limit");
+    limit.innerHTML = "You have 5 second to cancel your order";
+    status.innerText = "Order placed!";
+    document.getElementById("cancelButton").style.display = "block";
 
-
-function placeorder(){
-    var status = document.getElementById("Status");
-    status.innerText = "placed order";
+    setTimeout(() => {
+        if (!orderedCancle){ 
+            orderPreparation();
+        }
+    }, 4000);
+    setTimeout(() => {
+        if (!orderedCancle){ 
+            delivered();
+        }
+    }, 9000);
 }
+    
+function ordercancle(){
+    orderedCancle = true;
+    document.getElementById("status").innerHTML = "Order cancle";
+    document.getElementById("orderPrep").innerHTML = " ";
+    document.getElementById("delivery").innerHTML = " ";
+    document.getElementById("limit").innerHTML = " ";
+    document.getElementById("cancelButton").style.display = "none";
+
+}
+
+    
+function orderPreparation(){
+    if(orderedCancle === false){
+        document.getElementById("cancelButton").style.display = "none";
+        orderPrep.innerText="Order is in preparation period";
+        document.getElementById("limit").innerHTML = " ";
+
+
+    }
+
+}
+    
+function delivered(){
+    if(orderedCancle === false){
+        document.getElementById("delivery").innerText="Order is on the way";
+        document.getElementById("limit").innerHTML = " ";
+        document.getElementById("cancelButton").style.display = "none";
+        // document.getElementById("status").innerHTML = " ";
+        // document.getElementById("orderPrep").innerHTML = " ";
+    }
+        
+ }
 
