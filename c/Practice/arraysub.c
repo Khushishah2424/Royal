003210cb0e4a6ef3a44782053913ee09881a3b2f
Khushishah2424 <khushiashah2004@gmail.com>
2024-03-12 15:59:46 +0530

    ...

--- c/Practice/arraysub.c
+++ c/Practice/arraysub.c
@@ -10,9 +10,36 @@
 // Input: nums = [1,5,0,3,5] { when u subtract u have to subtract with all but not if its zero }
 
 // 1 5 0 3 5 
-// -1 
+// -1 .
 // 0 4 0 2 4 
 // -2 
 // 0 2 0 0 2 
 // -2 
 // 0 0 0 0  0
+#include<stdio.h>
+
+int main(){
+    int SIZE ;
+    printf("Enter SIZE :");
+    scanf("%d",&SIZE);
+
+    int a[SIZE];
+    for(int i = 0 ; i < SIZE ; i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i = 0 ; i < SIZE ; i++){
+        printf("%d",a[i]);
+    }
+
+    int min = a[0]; //1
+    for(int i = 0 ; i < SIZE ; i++){
+        if(a[i] == 0){
+            continue;
+        }
+        if(min == 0 || min>a[i]){
+            min = a[i];
+        }
+    }
+    printf("\nmin = %d",min);
+}
+
