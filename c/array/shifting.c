dac14a0af2840f4131d992577d052e25854f1a4d
Khushishah2424 <khushiashah2004@gmail.com>
2024-03-16 22:52:10 +0530

    ...

--- c/array/shifting.c
+++ c/array/shifting.c
@@ -1,32 +1,3 @@
-#include<stdio.h>
-#define n 5
-int main(){
-    int a[n],i,temp,shift;
-    printf("Enter the %d integers: \n",n);
-    for(i = 0 ;i < n ; i++){
-        scanf("%d",&a[i]);
-    }
-    printf("Enter howmany times you want to shift arrya :");
-    scanf("%d",&shift);
-
-
-    while(shift)
-    {
-        temp = a[0];
-         for( i = 0 ;i < n ; i++){
-            a[i]=a[i+1];
-         
-            a[n-1]=temp;  
-            shift--;  
-    }
-
-    for( i = 0 ;i < n ; i++){
-        printf(" %d ",a[i]);
-    }
-    return 0;
-    }
-}
-
 // #include<stdio.h>
 // #define n 5
 // int main(){
@@ -35,24 +6,53 @@ int main(){
 //     for(i = 0 ;i < n ; i++){
 //         scanf("%d",&a[i]);
 //     }
-//     printf("Enter howmany times you want to shift arrya :",shift);
+//     printf("Enter howmany times you want to shift arrya :");
 //     scanf("%d",&shift);
-//     while(shift)
-//     { 
-//         temp = a[n-1];//5
-//          for( i = n-1 ;i > 0  ; i--){//
-//             a[i]=a[i-1];//
 
-//          }
-//             a[0]=temp;
+
+//     while(shift)
+//     {
+//         temp = a[0];
+//          for( i = 0 ;i < n ; i++){
+//             a[i]=a[i+1];
+         
+//             a[n-1]=temp;  
 //             shift--;  
 //     }
-    
+
 //     for( i = 0 ;i < n ; i++){
 //         printf(" %d ",a[i]);
 //     }
-
+//     return 0;
+//     }
 // }
+
+#include<stdio.h>
+#define n 5
+int main(){
+    int a[n],i,temp,shift;
+    printf("Enter the %d integers: \n",n);
+    for(i = 0 ;i < n ; i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter howmany times you want to shift arrya :",shift);
+    scanf("%d",&shift);
+    while(shift)
+    { 
+        temp = a[n-1];//5
+         for( i = n-1 ;i > 0  ; i--){//
+            a[i]=a[i-1];//
+
+         }
+            a[0]=temp;
+            shift--;  
+    }
+    
+    for( i = 0 ;i < n ; i++){
+        printf(" %d ",a[i]);
+    }
+
+}
 
 
 
